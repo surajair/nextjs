6f542b4d1dd53726b91ecfecf6d059178d75a6fd
Suraj A <surajair@gmail.com>
2024-01-16 20:08:42 +0530

    feat: added pagelock support

--- app/api/chaibuilder/[...path]/route.ts
+++ app/api/chaibuilder/[...path]/route.ts
@@ -1,29 +1,36 @@
 import { NextRequest, NextResponse } from "next/server";
 import { revalidateTag } from "next/cache";
 import { chaiBuilder } from "@/app/chaiBuilder";
+import { headers } from "next/headers";
+
+function setAccessToken() {
+  const allHeaders = headers();
+  const accessToken = allHeaders.get("x-chai-access-token");
+  chaiBuilder.setAccessToken(accessToken);
+}
 
 export async function GET(_request: NextRequest, { params }: { params: any }) {
+  setAccessToken();
   const path = params.path.join("/");
   const { searchParams } = _request.nextUrl;
   switch (path) {
     case "project":
-      const { data: project } = await chaiBuilder.getProject();
+      const project = await chaiBuilder.getProject();
       return NextResponse.json(project);
     case "pages":
-      const { data: pages } = await chaiBuilder.getPages();
+      const pages = await chaiBuilder.getPages();
       return NextResponse.json(pages);
     case "page":
       const uuid = searchParams.get("uuid") as string;
-      const { data: page } = await chaiBuilder.getPage(uuid);
+      const page = await chaiBuilder.getPage(uuid);
       return NextResponse.json(page);
     case "verify":
-      const token = searchParams.get("token") as string;
-      const response = await chaiBuilder.verify(token);
+      const response = await chaiBuilder.verify();
       return NextResponse.json(response);
     case "assets":
       const limit = searchParams.get("limit") as string;
       const offset = searchParams.get("offset") as string;
-      const { data: assets } = await chaiBuilder.getAssets(limit, offset);
+      const assets = await chaiBuilder.getAssets(limit, offset);
       return NextResponse.json(assets);
     default:
       return NextResponse.json({});
@@ -31,6 +38,7 @@ export async function GET(_request: NextRequest, { params }: { params: any }) {
 }
 
 export async function POST(_request: NextRequest, { params }: { params: any }) {
+  setAccessToken();
   const path = params.path.join("/");
   switch (path) {
     case "publish":
@@ -39,18 +47,23 @@ export async function POST(_request: NextRequest, { params }: { params: any }) {
       return NextResponse.json({ result: "success" });
     case "pages":
       const pageData = await _request.json();
-      const { data: page } = await chaiBuilder.addPage(pageData);
+      const page = await chaiBuilder.addPage(pageData);
       return NextResponse.json(page);
-    case "authenticate":
-      const authData = await _request.json(); // { password }
-      const { data, error } = await chaiBuilder.authenticate(authData);
-      return NextResponse.json(data ? data : error, {
-        status: data ? 200 : 401,
-      });
+    case "login":
+      const loginFormData = await _request.json(); // { password }
+      const loginData = await chaiBuilder.login(loginFormData);
+      return NextResponse.json(loginData);
+    case "logout":
+      const logoutResponse = await chaiBuilder.logout();
+      return NextResponse.json(logoutResponse);
     case "upload":
       const uploadData = await _request.formData();
-      const { data: upload } = await chaiBuilder.uploadAsset(uploadData);
-      return NextResponse.json(upload);
+      const uploadedAsset = await chaiBuilder.uploadAsset(uploadData);
+      return NextResponse.json(uploadedAsset);
+    case "take-control":
+      const { uuid } = await _request.json();
+      const response = await chaiBuilder.unlockPage(uuid);
+      return NextResponse.json(response);
     default:
       return NextResponse.json({});
   }
@@ -58,16 +71,17 @@ export async function POST(_request: NextRequest, { params }: { params: any }) {
 
 //
 export async function PUT(_request: NextRequest, { params }: { params: any }) {
+  setAccessToken();
   const path = params.path.join("/");
   switch (path) {
     case "page":
       const pageData = await _request.json();
-      await chaiBuilder.updatePage(pageData);
-      return NextResponse.json({});
+      const response = await chaiBuilder.updatePage(pageData);
+      return NextResponse.json(response);
     case "project":
       const projectData = await _request.json();
-      await chaiBuilder.updateProject(projectData);
-      return NextResponse.json(projectData);
+      const project = await chaiBuilder.updateProject(projectData);
+      return NextResponse.json(project);
     default:
       return NextResponse.json({});
   }
@@ -77,12 +91,13 @@ export async function DELETE(
   _request: NextRequest,
   { params }: { params: any },
 ) {
+  setAccessToken();
   const path = params.path.join("/");
   switch (path) {
     case "page":
       const { searchParams } = _request.nextUrl;
       const uuid = searchParams.get("uuid") as string;
-      const { data: page } = await chaiBuilder.deletePage(uuid);
+      const page = await chaiBuilder.deletePage(uuid);
       return NextResponse.json(page);
     default:
       return NextResponse.json({});
